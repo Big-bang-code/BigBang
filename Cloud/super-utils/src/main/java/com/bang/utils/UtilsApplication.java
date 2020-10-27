bcf59d536d27d46b0f45386e88da53ca1303d61a
541676086@qq.com <541676086@qq.com>
2020-10-27 11:49:17 +0800

    对各模块的yml进行测试，预计准备增加全链路日志追踪功能

--- Cloud/super-utils/src/main/java/com/bang/utils/UtilsApplication.java
+++ Cloud/super-utils/src/main/java/com/bang/utils/UtilsApplication.java
@@ -1,6 +1,15 @@
 package com.bang.utils;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@SpringBootApplication
+@EnableScheduling
 public class UtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilsApplication.class,args);
+    }
+
 }
